4ec6569581a77ddf0910f1eed9969b0a9aaee6f8
toidiu <apoorv@toidiu.com>
2022-08-04 22:49:16 -0700

    detect trivial diff

--- src/main.rs
+++ src/main.rs
@@ -2,30 +2,71 @@ use std::fs::File;
 use std::io::{BufRead, BufReader, Write};
 use std::path::{Path, PathBuf};
 
+use glob::glob;
+
+const BASE_PATH: &str = "comparea";
+
 fn main() {
     let reader = BufReader::new(File::open("git_diff").expect("Cannot open git_diff"));
-
-    let base_path = Path::new("compare");
-
+    let base_path = Path::new(BASE_PATH);
     let mut current_file_path = Path::new("").to_owned();
 
     // seperate diff file into smaller files based
-    for line in reader.lines() {
-        let line = line.unwrap();
-        seperate_diff_into_individual_files(line, base_path, &mut current_file_path)
+    // let reader = BufReader::new(File::open("git_diff").expect("Cannot open git_diff"));
+    // for line in reader.lines() {
+    //     let line = line.unwrap();
+    //     seperate_diff_into_individual_files(line, &mut current_file_path)
+    // }
+
+    let glob_path = format!("{}/*", BASE_PATH);
+    for file_path in glob(&glob_path).unwrap().filter_map(Result::ok) {
+        let is_trivial = is_trivial_diff(&file_path);
+        println!(
+            "------trivial: {} {}",
+            is_trivial,
+            file_path.as_path().display()
+        );
     }
 }
 
-fn seperate_diff_into_individual_files(
-    line: String,
-    base_path: &Path,
-    current_file_path: &mut PathBuf,
-) {
+fn is_trivial_diff(file_path: &Path) -> bool {
+    let file = File::open(file_path).unwrap();
+    let reader = BufReader::new(file);
+
+    let mut is_trivial = true;
+
+    for line in reader.lines() {
+        if let Ok(line) = line.as_ref() {
+            if !is_interesting_diff(line) {
+                continue;
+            }
+        }
+
+        is_trivial = false;
+    }
+
+    is_trivial
+}
+
+fn is_interesting_diff(line: &str) -> bool {
+    let mut interesting = line.starts_with('-') || line.starts_with('+');
+
+    // &'static core::panic::Location<'static>
+    // &'static core::panic::location::Location<'static>
+    interesting &= !(line.contains("panic::") && line.contains("::Location"));
+    println!("panic:{} {} {}", !line.contains("panic"), interesting, line,);
+    interesting
+}
+
+fn seperate_diff_into_individual_files(line: String, current_file_path: &mut PathBuf) {
     if line.starts_with("@@") {
         let write_file = line.split("@@").last().unwrap().trim().replace(' ', "_");
         println!("{}", write_file);
+
+        let base_path = Path::new(BASE_PATH);
         *current_file_path = base_path.join(write_file);
         File::create(current_file_path.clone()).unwrap();
+        return;
     }
 
     let mut file = File::options()
